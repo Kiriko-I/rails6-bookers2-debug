af2b5ad7586762f0f725453475c591e0bc83a3ee
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-04-02 01:28:27 +0000

    [Add]応用課題7a

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,11 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
+    @comment.user_id = current_user.id
     @comment.save
   end
 
