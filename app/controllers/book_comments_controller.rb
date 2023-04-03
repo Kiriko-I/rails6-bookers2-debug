eb2d3e046082d545d1bdeabb0760a8346d3d0e49
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-04-03 10:53:26 +0000

    [Add]応用課題６非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -10,7 +10,8 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    BookComment.find(params[:book_id]).destroy
+    @comment = BookComment.find(params[:book_id])
+    @comment.destroy
   end
 
   private
